af01abdac5b6377bab7c151935735523cac28010
willschipp <will.schipp@gmail.com>
2017-10-26 09:13:24 +0400

    * .gitignore: initial commit
    * simple-db-rest-api.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * UnitTest1.cs:
    * TodoItem.cs:
    * TodoContext.cs:
    * simple-db-rest-api.csproj:
    * appsettings.Development.json:
    * TodoController.cs:
    * launchSettings.json:
    * ValuesController.cs:
    * simple-db-rest-api-test.csproj:

---
+++ simple-db-rest-api-test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+using TodoApi;
+
+namespace simple_db_rest_api_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //var obj = new TodoApi.Controllers.TodoController();
+        }
+    }
+}
---
+++ simple-db-rest-api/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+using TodoApi.Models;
+
+namespace TodoApi.Controllers {
+    
+    [Route("api/[controller]")]
+    public class TodoController : Controller {
+    
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0) 
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll() 
+        {
+            return _context.TodoItems.ToList();
+        }
+    }
+}
